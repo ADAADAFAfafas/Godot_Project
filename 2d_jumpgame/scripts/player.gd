c650c3884de8b432f8a8843440d58cc49b1e380f
Oreo <294457517@qq.com>
2025-06-10 23:52:05 +0800

    2D-demo-day2

--- 2d_jumpgame/scripts/player.gd
+++ 2d_jumpgame/scripts/player.gd
@@ -1,8 +1,30 @@
 class_name Player extends CharacterBody2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @export var Speed : float
-var direction : float
-
+@export var direction : float:
+	set(v):
+		direction = v
+		if last_direction != direction and direction != 0:
+			last_direction = direction
+@export var last_direction : float
+@export var is_player_2: bool = false
 func _physics_process(delta: float) -> void:
-	velocity.y += 98
+	velocity.y += 9.8
+	velocity.x = direction * Speed
 	move_and_slide()
+	
+func _process(delta: float) -> void:
+	direction = Input.get_axis("player_1_left","player_1_right") if not is_player_2 else Input.get_axis("player_2_left","player_2_right")
+	
+	if velocity.x !=0:
+		animation_player.play("run")
+	elif  velocity.x == 0:
+		animation_player.play("idle")
+	
+	if direction == -1:
+		rotation = 0
+		scale = Vector2(-1,1)
+	elif direction == 1:
+		rotation = 0
+		scale = Vector2(1,1)
